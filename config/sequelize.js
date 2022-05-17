d286d72317b6d4fd3ba856c2f2bfe19234f4ec3d
David Maman <dmaman86@gmail.com>
2022-05-17 12:51:03 +0300

    update sequelize config

--- config/sequelize.js
+++ config/sequelize.js
@@ -7,14 +7,7 @@ const sequelize = new Sequelize(
     database, username, password, {
         host: config.db.host,
         dialect: config.db.dialect,
-        native: true,  //adding this maybe breaks on hobby dyno
-        ssl: true, 
-        dialectOptions: {
-            ssl: {
-                require: true, // This will help you. But you will see nwe error
-                rejectUnauthorized: false // This line will fix new error
-            }
-        }
+        ssl: true
     }
 );
 
