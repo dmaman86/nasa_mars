57119eb606228b4c094511834e275585776ff211
David Maman <dmaman86@gmail.com>
2022-05-17 13:42:24 +0300

    update sequelize 3

--- config/sequelize.js
+++ config/sequelize.js
@@ -8,7 +8,6 @@ let sequelize;
 
 if(process.env.NODE_ENV === 'production'){
     sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
         dialectOptions:{
             ssl: true
         }
