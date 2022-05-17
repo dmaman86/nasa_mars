e97b6421f673057418f2b9842464dd6040763b85
David Maman <dmaman86@gmail.com>
2022-05-17 13:39:11 +0300

    update sequelize config 2

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,14 +1,33 @@
 const config = require("./config");
 const Sequelize = require("sequelize");
 
-const { database, username, password, host, dialect } = config.db;
+require('dotenv').config({path: 'variables.env'});
 
-const sequelize = new Sequelize(
+const { database, username, password, host, dialect } = config.db;
+let sequelize;
+
+if(process.env.NODE_ENV === 'production'){
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectOptions:{
+            ssl: true
+        }
+    });
+}else{
+    sequelize = new Sequelize(
+        database, username, password, {
+            host: host,
+            dialect: dialect,
+            ssl: true
+        }
+    )
+}
+/*const sequelize = new Sequelize(
     database, username, password, {
         host: host,
         dialect: dialect,
         ssl: true
     }
-);
+);*/
 
 module.exports = sequelize;
